9d2846f6231932c8c0c78185397c227c1e139727
Ritika <ritika3225@gmail.com>
2024-07-31 16:36:12 +0530

    hotel project files

--- hotel/app/Config/Routes.php
+++ hotel/app/Config/Routes.php
@@ -13,3 +13,17 @@ $routes->get('/login', 'Login::index');
 /*----Admin Login ------ */
 $routes->get('/admin', 'Admin::index');
 $routes->get('/admin/dashboard', 'Admin::dashboard');
+
+/*This is deskapp maniputation*/
+// Auth
+$routes->get('/auth', 'Auth::index');
+/*--------------------------------------------------------------------------- */
+$routes->group('admin', static function ($routes) {
+    $routes->group('', [], static function ($routes) {
+        $routes->view('example-page', 'example-page');
+    });
+
+    $routes->group('', [], static function ($routes) {
+        $routes->view('example-auth', 'example-auth');
+    });
+});
--- hotel/app/Controllers/Admin.php
+++ hotel/app/Controllers/Admin.php
@@ -6,43 +6,45 @@ class Admin extends BaseController
 {
     public function index()
     {
-        return view('admin/login');
-    }
+        // return view('admin/login');
 
-    public function authenticate()
-    {
-        $session = session();
-        $model = new UserModel();
-
-        $username = $this->request->getVar('username');
-        $password = $this->request->getVar('password');
-
-        $data = $model->where('username', $username)->first();
-
-        if ($data) {
-            $cpass = $data['password'];
-            $verify_pass = password_verify($password, $cpass);
-            if ($verify_pass) {
-                $ses_data = [
-                    'id' => $data['id'],
-                    'username' => $data['username'],
-                    'logged_in' => TRUE
-                ];
-                $session->set($ses_data);
-                return redirect()->to('/dashboard');
-            } else {
-                $session->setFlashdata('msg', 'Wrong Password');
-                return redirect()->to('/admin/login');
-            }
-        } else {
-            $session->setFlashdata('msg', 'Username not found');
-            return redirect()->to('/admin/login');
-        }
     }
 }
+
+//     public function authenticate()
+//     {
+//         $session = session();
+//         $model = new UserModel();
+
+//         $username = $this->request->getVar('username');
+//         $password = $this->request->getVar('password');
+
+//         $data = $model->where('username', $username)->first();
+
+//         if ($data) {
+//             $cpass = $data['password'];
+//             $verify_pass = password_verify($password, $cpass);
+//             if ($verify_pass) {
+//                 $ses_data = [
+//                     'id' => $data['id'],
+//                     'username' => $data['username'],
+//                     'logged_in' => TRUE
+//                 ];
+//                 $session->set($ses_data);
+//                 return redirect()->to('/dashboard');
+//             } else {
+//                 $session->setFlashdata('msg', 'Wrong Password');
+//                 return redirect()->to('/admin/login');
+//             }
+//         } else {
+//             $session->setFlashdata('msg', 'Username not found');
+//             return redirect()->to('/admin/login');
+//         }
+//     }
+// }
 /* public function dashboard()
  {
      return view('admin/dashboard');
 
  }
-}*/
+}
